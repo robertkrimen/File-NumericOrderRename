073e2774a809856e263b95ff5b547e24e22223fb
robertkrimen <robertkrimen@gmail.com>
2010-01-19 21:01:02 -0800

    Initial commit

---
+++ lib/File/NumericOrderRename.pm
@@ -0,0 +1,80 @@
+package File::NumericOrderRename;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+File::NumericOrderRename -
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+=head1 AUTHOR
+
+Robert Krimen, C<< <rkrimen at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-file-numericorderrename at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=File-NumericOrderRename>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc File::NumericOrderRename
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=File-NumericOrderRename>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/File-NumericOrderRename>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/File-NumericOrderRename>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/File-NumericOrderRename/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2010 Robert Krimen.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of File::NumericOrderRename
